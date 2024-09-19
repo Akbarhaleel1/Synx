89b6270093fafe849f65092a97acebb74907bbf7
kriparajp1 <kripp.me@outlook.com>
2024-09-20 00:51:43 +0530

    disable

--- Backend/synx/components/reviewscrapper.js
+++ Backend/synx/components/reviewscrapper.js
@@ -206,7 +206,7 @@ const trustpilot= async (url) => {
 // booking.com.........
 const booking= async (url) => {
 
-    const browser = await puppeteer.launch({headless: true});
+    const browser = await puppeteer.launch({headless: true,args: ['--no-sandbox', '--disable-setuid-sandbox']});
     const page = await browser.newPage();
     
     try {
