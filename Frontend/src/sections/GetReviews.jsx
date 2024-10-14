b9002bfd70d704aae94a5ae606afcab11632623d
Akbar Haleel <akbarhaleel508@gmail.com>
2024-10-14 17:00:07 +0530

    commit added

--- Frontend/src/sections/GetReviews.jsx
+++ Frontend/src/sections/GetReviews.jsx
@@ -7,6 +7,7 @@ import {
   FaInfoCircle,
   FaCamera,
   FaMicrophone,
+  FaTrash,
 } from "react-icons/fa";
 import Nav from "../components/Nav";
 import axios from "axios";
@@ -278,8 +279,10 @@ const GetReviews = () => {
         <button
           onClick={handleAddLine}
           className="flex-1 flex items-center justify-center px-4 py-2 border border-gray-300 rounded-lg text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 transition duration-150 ease-in-out"
-        >
-          <Plus className="w-5 h-5 mr-2 text-gray-400" />
+        >              
+
+          {/* <Plus className="w-5 h-5 mr-2 text-gray-400" /> */}
+          <FaTrash/>
           Add Contact
         </button>
         <EnhancedSubmitButton
