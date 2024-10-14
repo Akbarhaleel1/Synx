cff8c4d780132f0a1920d69b585e1a7f5dcc9a4c
Akbar Haleel <akbarhaleel508@gmail.com>
2024-10-14 17:10:05 +0530

    test commit 30

--- Frontend/src/sections/GetReviews.jsx
+++ Frontend/src/sections/GetReviews.jsx
@@ -7,7 +7,6 @@ import {
   FaInfoCircle,
   FaCamera,
   FaMicrophone,
-  FaTrash,
 } from "react-icons/fa";
 import Nav from "../components/Nav";
 import axios from "axios";
@@ -112,7 +111,7 @@ const GetReviews = () => {
             Authorization: `Bearer ${token}`,
           },
         });
-        console.log("resultsss", result.data);
+        console.log("result", result.data.message);
         if (result.data.message === "Not Found") {
           navigate('/PricingTable')
           return
@@ -271,7 +270,7 @@ const GetReviews = () => {
             onClick={() => handleDeleteLine(index)}
             className="p-2 rounded-lg bg-red-500 hover:bg-red-600 transition duration-150 ease-in-out"
           >
-               <FaTrash/>
+            <Trash2 className="w-5 h-5 text-white" />
           </button>
         </div>
       ))}
@@ -279,10 +278,8 @@ const GetReviews = () => {
         <button
           onClick={handleAddLine}
           className="flex-1 flex items-center justify-center px-4 py-2 border border-gray-300 rounded-lg text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 transition duration-150 ease-in-out"
-        >              
-
+        >
           <Plus className="w-5 h-5 mr-2 text-gray-400" />
-      
           Add Contact
         </button>
         <EnhancedSubmitButton
