c845939b0750ebb38367115e951c80a09c54f701
Akbar Haleel <akbarhaleel508@gmail.com>
2024-10-16 00:48:03 +0530

    test commit46

--- Frontend/src/sections/components/PositiveFeedBack.jsx
+++ Frontend/src/sections/components/PositiveFeedBack.jsx
@@ -1,17 +1,25 @@
 
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import {useNavigate} from 'react-router-dom'
 
 const PositiveFeedBack = () => {
+    const [data, setData] = useState('');
+    const navigate = useNavigate()
     useEffect(()=>{
         const fetchLink = () =>{
             const endpoint = localStorage.getItem('endpoint');
             const result = axios.post('https://synxbackend.synxautomate.com/starFilterReviewLink',{endpoint});
             console.log('result is',result)
+            setData(result.data.reviewLink.starFilter)
         }
         fetchLink()
     },[])
 
+    const handleNavigation = () =>{
+        console.log('data',data)
+        navigate(data)
+    }
   return (
     <div className="flex flex-col md:flex-row h-screen bg-white">
       <div className="md:w-1/2 p-8 overflow-y-auto">
@@ -24,10 +32,11 @@ const PositiveFeedBack = () => {
           <p className="text-gray-600 mb-6 text-sm">
             Leave us a review, it will help us grow and better serve our customer like you.
           </p>
-
+        
           <p className="text-center text-sm text-gray-500 mt-4">
             Leave a public review
           </p>
+          <button onClick={handleNavigation}>Leave a review</button>
         </div>
       </div>
       <div className="md:w-1/2 bg-gray-100 hidden md:block">
