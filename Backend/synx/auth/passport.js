95359bc3085b638f9d7e6e17f4ff2675b90c604d
Akbar Haleel <akbarhaleel508@gmail.com>
2024-10-18 21:40:33 +0530

    google auth fixed

--- Backend/synx/auth/passport.js
+++ Backend/synx/auth/passport.js
@@ -38,7 +38,7 @@ const passportConfig = () => {
     passport.use(
         new GoogleStrategy(
             {
-                callbackURL: 'https://synx-review.synxautomate.com/auth/google/callback',
+                callbackURL: '/auth/google/callback',
                 clientID: process.env.GOOGLE_AUTH_CLIENT_ID,
                 clientSecret: process.env.GOOGLE_AUTH_CLIENT_SECRET,
             },
@@ -59,9 +59,9 @@ const passportConfig = () => {
                         const newUser = new User({
                             name: profile._json.given_name || "",
                             email: profile._json.email,
-                            // isBlocked: false,
+                            isBlocked: false,
                             isVerified: true,
-                            // profilePic: profile._json.picture || null,
+                            profilePic: profile._json.picture || null,
                             password: generateRandomPassword(12),
                             customerId:customer.id,
                         });
