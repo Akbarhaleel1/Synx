9517fb97a6a6d0d818675324c50263b984bd3e7f
Akbar Haleel <akbarhaleel508@gmail.com>
2024-10-21 02:50:01 +0530

    Google auth is set up; updated Login component

--- Frontend/src/components/Nav.jsx
+++ Frontend/src/components/Nav.jsx
@@ -14,7 +14,7 @@ const Nav = () => {
   useEffect(() => {
     const getUser = localStorage.getItem('user');
     const user = JSON.parse(getUser);
-    console.log('user is', user);
+    console.log('user is', user.companyname);
     if(!user.companyname){
       setCompanyModal(true); 
       // alert('company name is not found')
