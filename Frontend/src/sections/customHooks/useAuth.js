883f35c5efe04d3bd4e37e942c6c80a99303f584
Akbar Haleel <akbarhaleel508@gmail.com>
2024-10-21 00:24:23 +0530

    Google auth is set up; updated Login component

--- Frontend/src/sections/customHooks/useAuth.js
+++ Frontend/src/sections/customHooks/useAuth.js
@@ -4,13 +4,22 @@ import { useNavigate } from 'react-router-dom';
 
 const useAuth = () => {
     const navigate = useNavigate();
+    const params = new URLSearchParams(window.location.search);
+    const userData = params.get('userData');
+    const tokens = params.get('token');
+    const refreshToken = params.get('refreshToken');
+
+    console.log('User Data111111111111111111111:', userData);
+    console.log('Token111111111111111:', decodeURIComponent(tokens));
+    console.log('Refresh Token:111111', decodeURIComponent(refreshToken));
 
     useEffect(() => {
         const manageTokens = async () => {
-            let accessToken = JSON.parse(localStorage.getItem('token'));
-
+            accessToken = JSON.parse(localStorage.getItem('token'));
+            console.log('accessToken',accessToken)
             if (isTokenExpired(accessToken)) {
                 try {
+                    console.log('refreshAccessToken')
                     await refreshAccessToken(); 
                     accessToken = JSON.parse(localStorage.getItem('token'));
                 } catch (error) {
