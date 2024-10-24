f150e776f9e613d1e6e54bdb0c7d89d22017754d
Akbar Haleel <akbarhaleel508@gmail.com>
2024-10-25 01:46:15 +0530

    change the url name

--- Frontend/src/sections/EditReviews.jsx
+++ Frontend/src/sections/EditReviews.jsx
@@ -91,7 +91,7 @@ const EditReviews = () => {
   //   navigate(`/HotelReview?endpoint=${encodeURIComponent(endpoint)}$tab=text`);
   // };
   const handleVisitLink = () => {
-    window.open(`https://synx-review.synxautomate.com/Reviews?point=${encodeURIComponent(endpoint)}&tab=text}`, '_blank');
+    window.open(`https://synx-review.synxautomate.com/Reviewssss?point=${encodeURIComponent(endpoint)}&tab=text}`, '_blank');
     // navigate(`/HotelReview?endpoint=${encodeURIComponent(endpoint)}&tab=text`);
   };
 
