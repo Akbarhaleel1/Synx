b9140d56b7f80e1bd34a249d5f1d9e4f391da367
Akbar Haleel <akbarhaleel508@gmail.com>
2024-10-25 02:13:30 +0530

    change the url name

--- Frontend/src/sections/Reviews.jsx
+++ Frontend/src/sections/Reviews.jsx
@@ -221,7 +221,7 @@ const Reviews = () => {
       </p>
       
       {/* Add a subtle button encouraging engagement */}
-     <a href="https://synx-review.synxautomate.com/integrations">
+     <a href="https://review.synxautomate.com/integrations">
       <button
         className="mt-6 px-6 py-2 bg-gradient-to-r from-indigo-500 to-pink-500 text-white text-sm font-medium rounded-full shadow-md hover:shadow-lg transition-transform transform hover:scale-105"
       >
